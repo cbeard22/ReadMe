dae1074fb920418b373f78a7abae207148169397
Chris Beard <cbeard@hnoj.org>
2021-06-11 12:20:32 -0500

    basic readme layout but not styled or badges

--- index.js
+++ index.js
@@ -1,32 +1,120 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-const generateHTML = (answers) =>
-    `<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset = "UTF-8">
-<meta http-equiv="X-UA-Compatible" contant="ie=edge">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<title>Read Me</title>
-</head>
-<body>
-
-</body>
-</html>`;
+//const outputCyanText = (text) => console.log(`\x1b[36m${text}\x1b[0m`);
 
 inquirer
     .prompt([
         {
-            type: "",
-            name: "",
-            message: "",
-        }
+            type: "input",
+            name: "title",
+            message: "What is the title of your application?",
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "Write a brief description of what your application does:",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required for installing your project?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide some brief instuctions on how to use your application:",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project: ",
+            choices: [
+                "Apache-2.0",
+                "BSD-3-Clause",
+                "BSD-2-Clause",
+                "ISC",
+                "MIT",
+                "MPL-2.0",
+                "EPL-2.0",
+                "N/A",
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "If you would like others developers to contribute specify your guidelines on how to do so here:",
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: "Please list any test instructions for your program:",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email as a way to contact you if the user has any quesitions: "
+        },
     ])
 
-.then((answers) => {
-    const htmlPageContent = generateHTML(answers);
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-    err ? console.log(err) : console.log("Your ReadMe was created successfully!")
-    );
-});
+.then(({
+    title ="/x1b[30m",
+    Description ="/x1b[30m",
+    usage,
+    license,
+    contributing,
+    Tests,
+    username,
+    email,
+    
+})=>{
+    const template = 
+    `# ${title}
+
+    * [Description](#Description)
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    
+    ## Description
+    ${Description}
+
+    ## Usage
+    ${usage}
+
+    ## License
+        For more information about the license, please click on the link below.
+
+    -[License](http:opensource.org/licenses/${license})
+
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${Tests}
+
+    ## Questions
+    * GitHub: ${username}
+    For any questions please contact me at my email: ${email}`
+    
+    createNewFile(title,template);
+}
+);
+
+function createNewFile(fileName,data){
+
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`,data,(err)=>{
+        if (err){
+            console.log(err)
+        }
+        console.log("Your ReadMe file was created Successfully!")
+    })
+};
+
